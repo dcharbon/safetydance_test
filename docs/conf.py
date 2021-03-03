7b3fdfba565b490a24cb1e15158a49ea56bdbe4d
Abraham Maxfield <utabe11@hotmail.com>
2021-03-03 20:33:58 +0000

    Update conf.py replace "llstep" with "safetydance"

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../src/llstep_test")
+module_dir = os.path.join(__location__, "../src/safetydance_test")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
@@ -81,7 +81,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'llstep_test'
+project = u'safetydance_test'
 copyright = u'2019, David Charboneau'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -151,7 +151,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 try:
-    from llstep_test import __version__ as version
+    from safetydance_test import __version__ as version
 except ImportError:
     pass
 else:
@@ -216,7 +216,7 @@ html_static_path = ['_static']
 # html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'llstep_test-doc'
+htmlhelp_basename = 'safetydance_test-doc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -235,7 +235,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'user_guide.tex', u'llstep_test Documentation',
+  ('index', 'user_guide.tex', u'safetydance_test Documentation',
    u'David Charboneau', 'manual'),
 ]
 
